a428ffdcede6331f345dc7015cfa2044a67a87ff
ijestanoff <jestanoff@icloud.com>
2024-07-04 18:04:13 +0300

    Options set

--- python_fundamentals/Dictionaries_More_Exercises/test.py
+++ python_fundamentals/Dictionaries_More_Exercises/test.py
@@ -1,9 +1,1 @@
-footballers_goals = {'Eusebio': 120, 'Cruyff': 120, 'Pele': 150, 'Ronaldo': 132, 'Messi': 125}
-footballers_goals = dict(sorted(footballers_goals.items(), key=lambda x: (-x[1], x[0])))
-
-print(footballers_goals)
-
-a = "Kolio"
-b = "Gosho"
-print(id(a))
-print(id(b))
+> "".join(str(n) for n in range(1, 10000)).index('4949')
